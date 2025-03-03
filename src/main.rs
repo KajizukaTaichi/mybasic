e99cd0601a166a20b2e547305891e4bdb7dc33be
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-03 22:58:20 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,25 @@ fn main() {
 }
 
 struct Compiler {
-    variables: BTreeMap<String, Value>,
-    precode: String,
+    variables: Vec<String>,
 }
 
 fn compile_main(source: &str) -> Option<String> {
-    let mut program = BTreeMap::new();
-    for line in source.lines() {
-        let line = line.trim();
-        let (ln, code) = line.split_once(SPACE[0])?;
-        let ln: usize = ln.trim().parse().unwrap();
-        program.insert(ln, Stmt::parse(code.trim())?);
+    let program = {
+        let mut program = BTreeMap::new();
+        for line in source.lines() {
+            let line = line.trim();
+            let (ln, code) = line.split_once(SPACE[0])?;
+            let ln: usize = ln.trim().parse().unwrap();
+            program.insert(ln, Stmt::parse(code.trim())?);
+        }
+        program
+    };
+
+    let mut result = String::new();
+    let mut ctx = Compiler { variables: vec![] };
+    for (line, code) in program {
+        result.push_str(&format!("line_{line}:\n{}\n\n", code.compile(&mut ctx)));
     }
     todo!()
 }
