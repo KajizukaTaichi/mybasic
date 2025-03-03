c9efe3c93bd05be9c44277a3dc7120197a4a4885
梶塚太智 <kajizukataichi@outlook.jp>
2025-03-03 22:26:58 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,27 @@ mod value;
 use expr::Expr;
 use lexer::{str_escape, tokenize};
 use oper::Oper;
+use stmt::Stmt;
 use util::{OPERATOR, SPACE, include_letter};
 use value::Value;
 
+use std::collections::BTreeMap;
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct Compiler {
+    variables: BTreeMap<String, Value>,
+}
+
+fn compile_main(source: &str) -> Option<String> {
+    let mut program = BTreeMap::new();
+    for line in source.lines() {
+        let line = line.trim();
+        let (ln, code) = line.split_once(SPACE[0])?;
+        let ln: usize = ln.trim().parse().unwrap();
+        program.insert(ln, Stmt::parse(code.trim())?);
+    }
+    todo!()
 }
