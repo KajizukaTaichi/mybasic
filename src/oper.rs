97fa0054df8ff55890fb7c5f0123e63b76a0d3be
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-27 17:43:13 +0900

    Update oper.rs

--- src/oper.rs
+++ src/oper.rs
@@ -24,13 +24,13 @@ impl Oper {
             let lhs = lhs.compile(ctx)?;
             let rhs = rhs.compile(ctx)?;
             Some(if lhs.contains("\n") && rhs.contains("\n") {
-                format!("{lhs}psh ar\n{rhs}mov dr, ar\npop ar\n{opecode} ar, dr\n")
+                format!("\t{lhs}psh ar\n\t{rhs}\tmov dr, ar\n\tpop ar\n\t{opecode} ar, dr\n")
             } else if lhs.contains("\n") {
-                format!("{lhs}{opecode} ar, {rhs}\n")
+                format!("\t{lhs}\t{opecode} ar, {rhs}\n")
             } else if rhs.contains("\n") {
-                format!("{rhs}\nmov dr, ar\nmov ar, {lhs}\n{opecode} ar, dr\n")
+                format!("\t{rhs}\nmov dr, ar\n\tmov ar, {lhs}\n\t{opecode} ar, dr\n")
             } else {
-                format!("mov ar, {lhs}\n{opecode} ar, {rhs}\n")
+                format!("\tmov ar, {lhs}\n\t{opecode} ar, {rhs}\n")
             })
         };
         Some(match self {
