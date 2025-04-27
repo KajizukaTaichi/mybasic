d928bc79ddf4d8d2df2d82390be82e02af46c904
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-27 17:44:24 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         label_index: 0,
         variables: IndexMap::new(),
     };
-    let code = "let a = 1\nif 1 then let a = 2\na * 10";
+    let code = "let a = 1\nif 0 then let a = 2\na * 10";
     let output = ctx.build(code).unwrap();
     println!("{output}");
     let bytecodes = asm(&output).unwrap();
