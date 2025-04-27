51fc0b34a404eef8f8ba28cde377dacade392d58
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-27 20:12:33 +0900

    Update expr.rs

--- src/expr.rs
+++ src/expr.rs
@@ -2,7 +2,7 @@ use crate::*;
 
 #[derive(Debug, Clone)]
 pub enum Expr {
-    Value(Value),
+    Value(f64),
     Refer(String),
     Oper(Box<Oper>),
 }
@@ -15,23 +15,26 @@ impl Expr {
             Some(Expr::Oper(Box::new(Oper::parse(source)?)))
         } else {
             let token = token_list.last()?.trim().to_string();
-            Some(if let Ok(n) = token.parse::<f64>() {
-                Expr::Value(Value::Num(n))
-            // prioritize higher than others
-            } else if token.starts_with("(") && token.ends_with(")") {
-                let token = token.get(1..token.len() - 1)?.trim();
-                Expr::parse(token)?
-            // Variable reference
-            } else {
-                Expr::Refer(token)
-            })
+            Some(
+                // Number literal
+                if let Ok(number) = token.parse::<f64>() {
+                    Expr::Value(number)
+                // prioritize higher than others
+                } else if token.starts_with("(") && token.ends_with(")") {
+                    let token = token.get(1..token.len() - 1)?.trim();
+                    Expr::parse(token)?
+                // Variable reference
+                } else {
+                    Expr::Refer(token)
+                },
+            )
         }
     }
 
     pub fn compile(&self, ctx: &mut Compiler) -> Option<String> {
         Some(match self {
             Expr::Oper(oper) => oper.compile(ctx)?,
-            Expr::Value(Value::Num(n)) => n.to_string(),
+            Expr::Value(number) => number.to_string(),
             Expr::Refer(to) => format!("\tlda ar, {}\n", ctx.variables.get(to)?),
         })
     }
